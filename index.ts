5aa9f63542f3d27878017af0b040d33fc2830b1f
Somya <isomya13@gmail.com>
2023-12-21 22:28:08 +0530

    feat: socket setup.

--- index.ts
+++ index.ts
@@ -1,11 +1,43 @@
-import {createServer} from "http";
+import { Socket } from "socket.io";
+const {Server} = require("socket.io");
+import {randomUUID} from 'crypto'
 
-const server = createServer();
-const wss = new WebSocket.Server({
-    server: server
+
+type Room = {
+   roomName: string,
+   users : string[],
+   roomid: string
+}
+
+const io = new Server(3000, {
+
 });
 
-wss.on("connection", (ws)=>{
-    
-})
+const rooms: Room[] = [];
 
+
+io.on('connection', (socket: Socket)=>{
+    
+    socket.on('create room', (roomName: string)=>{
+        rooms.push({
+            roomName: roomName,
+            users : [],
+            roomid: randomUUID()
+        });
+        socket.join(roomName);
+    });
+
+    socket.on('get rooms', ()=>{
+        socket.emit('rooms',
+        rooms.map((room: Room)=> ({name: room.roomName, id: room.roomid})
+        ));
+    });
+
+    socket.on('join room', (roomid)=>{
+       const room = rooms.find((room)=> room.roomid === roomid);
+       if(room){
+        room.users.push(socket.id)
+        socket.join(roomid);
+       }
+    })
+})
